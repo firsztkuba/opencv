2e4b6ea1b90b9b7436e862b685097bab87edb74a
Kuba Firszt <firszt.kuba@gmail.com>
2023-02-07 22:32:27 +0100

    work in progress

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import cv2
+
+img = cv2.imread("image.jpg", 0)
+img = cv2.resize(img, (int(img.shape[1]/2),int(img.shape[0]/2)))
+
+cv2.imshow("Days Gone", img)
+cv2.imwrite("Days Gone Grayscale.jpg", img)
+cv2.waitKey(5000)
+cv2.destroyAllWindows
