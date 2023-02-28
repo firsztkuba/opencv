2707dfb988de51db9a720ac29f6da9af2d6ca6be
Kuba Firszt <firszt.kuba@gmail.com>
2023-02-28 22:39:00 +0100

    adding funcionalities

--- video_face_detector.py
+++ video_face_detector.py
@@ -1,14 +1,19 @@
 import cv2, time
 
 video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+a=1
 
 while True:
     check, frame = video.read()
-
-    print(check)
-    print(frame)
-
+    a += 1
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.06, minNeighbors=4)
+
+
+    for x, y, w, h in faces:
+        gray = cv2.rectangle(gray, (x, y), (x+w, y+h), (0,255,0), 3)
     
     cv2.imshow("Capturing", gray)
 
@@ -16,6 +21,6 @@ while True:
 
     if key == ord("q"):
         break
-
+print(a)
 video.release()
 cv2.destroyAllWindows
